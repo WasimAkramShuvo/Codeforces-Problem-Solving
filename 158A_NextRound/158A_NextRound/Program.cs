3e9d352ac0c582819764d031bf319bf32be5d923
WasimAkramShuvo <wasimakramshuvo.net@gmail.com>
2023-06-04 03:08:09 +0600

    next round committed

---
+++ 158A_NextRound/158A_NextRound/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _158A_NextRound
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input1 = Console.ReadLine().Split();
+            var n = int.Parse(input1[0]);
+            var k = int.Parse(input1[1]);
+
+            var input2 = Console.ReadLine().Split();
+            var scores = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                scores[i] = int.Parse(input2[i]);
+            }
+
+            int count = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (scores[i] > 0 && scores[i] >= scores[k - 1])
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
