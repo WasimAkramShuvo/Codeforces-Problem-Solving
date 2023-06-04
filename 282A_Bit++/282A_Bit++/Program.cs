62755abaf77d61669e5cb97b8bd0ded0de1db5db
WasimAkramShuvo <wasimakramshuvo.net@gmail.com>
2023-06-05 02:51:29 +0600

    bit++ committed

---
+++ 282A_Bit++/282A_Bit++/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _282A_Bit__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = Int32.Parse(Console.ReadLine());
+            int x = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                var statements = Console.ReadLine();
+
+                if (statements == "X++" || statements == "++X")
+                    x++;
+                else if (statements == "X--" || statements == "--X")
+                    x--;
+            }
+
+            Console.WriteLine(x);
+        }
+    }
+}
