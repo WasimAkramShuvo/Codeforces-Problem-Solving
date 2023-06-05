36f33d1fc4ab14dfb61fcc923fabad9cfe8aab61
WasimAkramShuvo <wasimakramshuvo.net@gmail.com>
2023-06-05 23:42:42 +0600

    theatre square comiitted

---
+++ 1A_TheatreSquare/1A_TheatreSquare/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _1A_TheatreSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputs = Console.ReadLine().Split();
+            var n = long.Parse(inputs[0]);
+            var m = long.Parse(inputs[1]);
+            var a = long.Parse(inputs[2]);
+
+            var row = n/a;
+            if(n%a != 0)
+            {
+                row++;
+            }
+
+            var col = m/a;
+            if(m % a != 0)
+            {
+                col++;
+            }
+
+            Console.WriteLine(row*col);
+        }
+    }
+}
