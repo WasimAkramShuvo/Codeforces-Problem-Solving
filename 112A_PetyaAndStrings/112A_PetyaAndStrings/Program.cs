c53d955a7c9655cf28943646f8f00576bf771720
WasimAkramShuvo <wasimakramshuvo.net@gmail.com>
2023-06-08 00:13:22 +0600

    Petya and Strings committed

---
+++ 112A_PetyaAndStrings/112A_PetyaAndStrings/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _112A_PetyaAndStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = Console.ReadLine();
+            var y = Console.ReadLine();
+
+            var result = String.Compare(x, y, StringComparison.OrdinalIgnoreCase);
+
+            if(result < 0)
+                Console.WriteLine(-1);
+            else if(result > 0)
+                Console.WriteLine(1);
+            else
+                Console.WriteLine(0);
+        }
+    }
+}
